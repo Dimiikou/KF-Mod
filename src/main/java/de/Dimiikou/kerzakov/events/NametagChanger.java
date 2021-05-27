dfb29e5aa7056da33b7c0d1d49432c1463560ed3
Dimiikou <leon.aissa@icloud.com>
2021-05-27 16:10:51 +0200

    Added colored names for corpse

--- src/main/java/de/Dimiikou/kerzakov/events/NametagChanger.java
+++ src/main/java/de/Dimiikou/kerzakov/events/NametagChanger.java
@@ -1,20 +1,23 @@
 package de.Dimiikou.kerzakov.events;
 
 import de.Dimiikou.kerzakov.config.KerzakovConfig;
+import de.fuzzlemann.ucutils.Main;
+import de.fuzzlemann.ucutils.base.text.TextUtils;
 import de.fuzzlemann.ucutils.utils.faction.Faction;
 import de.fuzzlemann.ucutils.utils.faction.FactionPlayersList;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemSkull;
 import net.minecraft.scoreboard.ScorePlayerTeam;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
+import java.util.regex.Pattern;
 
 @Mod.EventBusSubscriber
 @SideOnly(Side.CLIENT)
@@ -22,9 +25,13 @@ public class NametagChanger {
 
     public static ArrayList<String> names = new ArrayList<>();
     private static final Map<String, EntityPlayer> PLAYER_MAP = new HashMap<>();
+    private static int tick;
+    private static final Pattern STRIP_PREFIX_PATTERN = Pattern.compile("\\[[0-9A-Za-z]+]");
 
     @SubscribeEvent
     public static void onNameFormat(PlayerEvent.NameFormat e) {
+        if (!KerzakovConfig.NametagChanger) return;
+
         EntityPlayer p = e.getEntityPlayer();
 
         String userName = e.getUsername();
@@ -39,13 +46,48 @@ public class NametagChanger {
 
         if (playersFaction != null && FactionPlayersList.PLAYER_TO_FACTION_MAP.get(userName) == playersFaction) {
             e.setDisplayname("\247" + KerzakovConfig.Farbcode + userName);
-        }
-
-        if (Faction.TRIADS != null && FactionPlayersList.PLAYER_TO_FACTION_MAP.get(userName) == Faction.TRIADS) {
+        } else if (FactionPlayersList.PLAYER_TO_FACTION_MAP.get(userName) == Faction.TRIADS) {
             e.setDisplayname("\247" + KerzakovConfig.Farbcode + userName);
         }
 
         names.add(userName);
+    }
+
+    // Danke Fuzzle haha
+    @SubscribeEvent
+    public static void onWorldTick(TickEvent.ClientTickEvent e) {
+        if (e.phase != TickEvent.Phase.START) return;
+
+        if (Main.MINECRAFT.world == null) return;
+        if (tick++ != 20) return;
+
+        List<EntityItem> items = Main.MINECRAFT.world.getEntities(EntityItem.class, (ent) -> ent != null && ent.hasCustomName() && ent.getItem().getItem() instanceof ItemSkull);
+
+        for (EntityItem entityItem : items) {
+            String name = entityItem.getCustomNameTag();
+            if (name.startsWith("§8")) continue; //Hitman Corpse
+
+            String colorStrippedName = name.substring(2);
+            String realName = colorStrippedName.substring(1);
+
+            String color = null;
+
+            if (Faction.getFactionOfPlayer() != null && FactionPlayersList.PLAYER_TO_FACTION_MAP.get(STRIP_PREFIX_PATTERN.matcher(realName).replaceAll("")) == Faction.getFactionOfPlayer()) {
+                color = "\247" + KerzakovConfig.Farbcode;
+            } else if (FactionPlayersList.PLAYER_TO_FACTION_MAP.get(STRIP_PREFIX_PATTERN.matcher(realName).replaceAll("")) == Faction.TRIADS) {
+                color = "\247" + KerzakovConfig.Farbcode;
+            }
+
+            if (color == null) {
+                if (name.startsWith("§7")) continue;
+
+                color = "§7";
+            }
+
+            entityItem.setCustomNameTag(color + colorStrippedName);
+        }
+
+        tick = 0;
     }
 
     @SubscribeEvent
